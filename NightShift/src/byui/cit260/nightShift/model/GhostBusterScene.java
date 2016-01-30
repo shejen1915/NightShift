5342e9e27118b9cae1499a9b2da548350a7dcb0f
Ni shi shei <Ni shi shei@10.18.148.120>
2016-01-30 16:17:31 -0700

    1-30-2016 Orange and Purple Class Instances added

--- NightShift/src/byui/cit260/nightShift/model/GhostBusterScene.java
+++ NightShift/src/byui/cit260/nightShift/model/GhostBusterScene.java
@@ -9,54 +9,16 @@ import java.util.Objects;
 
 /**
  *
- * @author Silver
+ * @author Ni shi shei
  */
 public class GhostBusterScene extends RegularScene{
     
-    // class instance variables
     private String callGhost;
     private String trapGhost;
     private String ghostEncounterNullify;
 
     public GhostBusterScene() {
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 11 * hash + Objects.hashCode(this.callGhost);
-        hash = 11 * hash + Objects.hashCode(this.trapGhost);
-        hash = 11 * hash + Objects.hashCode(this.ghostEncounterNullify);
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "GhostBusterScene{" + "callGhost=" + callGhost + ", trapGhost=" + trapGhost + ", ghostEncounterNullify=" + ghostEncounterNullify + '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final GhostBusterScene other = (GhostBusterScene) obj;
-        if (!Objects.equals(this.callGhost, other.callGhost)) {
-            return false;
-        }
-        if (!Objects.equals(this.trapGhost, other.trapGhost)) {
-            return false;
-        }
-        return Objects.equals(this.ghostEncounterNullify, other.ghostEncounterNullify);
-    }
-    
-    
 
     public String getCallGhost() {
         return callGhost;
@@ -81,6 +43,45 @@ public class GhostBusterScene extends RegularScene{
     public void setGhostEncounterNullify(String ghostEncounterNullify) {
         this.ghostEncounterNullify = ghostEncounterNullify;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.callGhost);
+        hash = 97 * hash + Objects.hashCode(this.trapGhost);
+        hash = 97 * hash + Objects.hashCode(this.ghostEncounterNullify);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GhostBusterScene other = (GhostBusterScene) obj;
+        if (!Objects.equals(this.callGhost, other.callGhost)) {
+            return false;
+        }
+        if (!Objects.equals(this.trapGhost, other.trapGhost)) {
+            return false;
+        }
+        if (!Objects.equals(this.ghostEncounterNullify, other.ghostEncounterNullify)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "GhostBusterScene{" + "callGhost=" + callGhost + ", trapGhost=" + trapGhost + ", ghostEncounterNullify=" + ghostEncounterNullify + '}';
+    }
+    
     
     
 }
