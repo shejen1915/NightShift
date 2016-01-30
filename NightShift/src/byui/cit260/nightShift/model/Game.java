1648d84e299310e4d38a73e6b512acf18abdb1bc
Ni shi shei <Ni shi shei@10.18.148.120>
2016-01-30 11:35:53 -0700

    1-30-2016 Orange Classes added

---
+++ NightShift/src/byui/cit260/nightShift/model/Game.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.nightShift.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ni shi shei
+ */
+public class Game implements Serializable{
+    
+    private double score;
+    private double startTime;
+
+    public Game() {
+    }
+    
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public double getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(double startTime) {
+        this.startTime = startTime;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.score) ^ (Double.doubleToLongBits(this.score) >>> 32));
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.startTime) ^ (Double.doubleToLongBits(this.startTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.score) != Double.doubleToLongBits(other.score)) {
+            return false;
+        }
+        return Double.doubleToLongBits(this.startTime) == Double.doubleToLongBits(other.startTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "score=" + score + ", startTime=" + startTime + '}';
+    }
+
+    public void setScore(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
