9ae12b2d3f94fe156d4845e9fc63a3ab5135ef58
Ni shi shei <Ni shi shei@10.18.148.120>
2016-01-30 16:17:13 -0700

    1-30-2016 Orange and Purple Class Instances added

--- NightShift/src/byui/cit260/nightShift/model/GoOutWindow.java
+++ NightShift/src/byui/cit260/nightShift/model/GoOutWindow.java
@@ -9,30 +9,48 @@ import java.util.Objects;
 
 /**
  *
- * @author Silver
+ * @author Ni shi shei
  */
 public class GoOutWindow extends UpDown{
     
-    // class instance variable
-    private String requireRope;
+    private String windowEquation;
+    private String requiresRope;
     private String gainGhostBuster;
-    private double windowEquation;
 
     public GoOutWindow() {
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 67 * hash + Objects.hashCode(this.requireRope);
-        hash = 67 * hash + Objects.hashCode(this.gainGhostBuster);
-        hash = 67 * hash + (int) (Double.doubleToLongBits(this.windowEquation) ^ (Double.doubleToLongBits(this.windowEquation) >>> 32));
-        return hash;
+    public String getWindowEquation() {
+        return windowEquation;
+    }
+
+    public void setWindowEquation(String windowEquation) {
+        this.windowEquation = windowEquation;
+    }
+
+    public String getRequiresRope() {
+        return requiresRope;
+    }
+
+    public void setRequiresRope(String requiresRope) {
+        this.requiresRope = requiresRope;
+    }
+
+    public String getGainGhostBuster() {
+        return gainGhostBuster;
+    }
+
+    public void setGainGhostBuster(String gainGhostBuster) {
+        this.gainGhostBuster = gainGhostBuster;
     }
 
     @Override
-    public String toString() {
-        return "GoOutWindow{" + "requireRope=" + requireRope + ", gainGhostBuster=" + gainGhostBuster + ", windowEquation=" + windowEquation + '}';
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.windowEquation);
+        hash = 97 * hash + Objects.hashCode(this.requiresRope);
+        hash = 97 * hash + Objects.hashCode(this.gainGhostBuster);
+        return hash;
     }
 
     @Override
@@ -47,40 +65,23 @@ public class GoOutWindow extends UpDown{
             return false;
         }
         final GoOutWindow other = (GoOutWindow) obj;
-        if (Double.doubleToLongBits(this.windowEquation) != Double.doubleToLongBits(other.windowEquation)) {
+        if (!Objects.equals(this.windowEquation, other.windowEquation)) {
             return false;
         }
-        if (!Objects.equals(this.requireRope, other.requireRope)) {
+        if (!Objects.equals(this.requiresRope, other.requiresRope)) {
             return false;
         }
-        return Objects.equals(this.gainGhostBuster, other.gainGhostBuster);
+        if (!Objects.equals(this.gainGhostBuster, other.gainGhostBuster)) {
+            return false;
+        }
+        return true;
     }
 
+    @Override
+    public String toString() {
+        return "GoOutWindow{" + "windowEquation=" + windowEquation + ", requiresRope=" + requiresRope + ", gainGhostBuster=" + gainGhostBuster + '}';
+    }
     
-    
-    public String getRequireRope() {
-        return requireRope;
-    }
-
-    public void setRequireRope(String requireRope) {
-        this.requireRope = requireRope;
-    }
-
-    public String getGainGhostBuster() {
-        return gainGhostBuster;
-    }
-
-    public void setGainGhostBuster(String gainGhostBuster) {
-        this.gainGhostBuster = gainGhostBuster;
-    }
-
-    public double getWindowEquation() {
-        return windowEquation;
-    }
-
-    public void setWindowEquation(double windowEquation) {
-        this.windowEquation = windowEquation;
-    }
     
     
 }
