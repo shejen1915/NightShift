9898ea9a63d578fd87f8f8f6bfeb5db8063ad5a9
Ni shi shei <Ni shi shei@10.18.148.120>
2016-02-12 09:43:23 -0700

    2-12-2016 GhostEncounterControl class updated and its Test added

--- NightShift/src/byui/cit260/nightShift/control/GhostEncounterControl.java
+++ NightShift/src/byui/cit260/nightShift/control/GhostEncounterControl.java
@@ -10,5 +10,36 @@ package byui.cit260.nightShift.control;
  * @author Ni shi shei
  */
 public class GhostEncounterControl {
-    
+    public double calcGhostEncounterProbability(double collectedItems, double collectableItems, double numberOfCharms, double superstitious, double unsuperstitious) {
+
+        //declared variables
+        double charmPoints = 1;
+        
+        double stair = 100;
+        
+        
+	if (stair != 100) {
+            return -1;
+        }
+        
+	if (collectedItems < 0 || collectedItems > 5) {
+            return -1;
+        }
+        
+	if(charmPoints < 0 || charmPoints > 10) {
+            return -1;
+        }    
+        
+
+        double itemBonus = ((collectedItems/collectableItems)/2) * 100;
+            //collectedItemBonus = ((collectedItems/possibleCollectedItems) / 2) * 100
+            
+        double charmBonus = ((numberOfCharms * charmPoints)/2) * 100;
+            //charmPoints = ((numberOfCharms * charmItemPoints)/2) *100
+                
+        double probability = stair - itemBonus - superstitious - unsuperstitious - charmBonus;
+            //ghostEncounterProbability = 100 – collectedItemBonus – (superstitiousCompanion OR unsuperstitiousCompanion) – (charmPoints)
+            return probability;
+    }
+
 }
