27bd327b9ec52cadacb21ad59c72443d4a5b6d82
Silver <Silver@TRON>
2016-03-25 04:34:33 -0600

    Merge origin/master
    Conflicts:
    	NightShift/src/nightshift/NightShift.java

--- NightShift/src/byui/cit260/nightShift/view/View.java
+++ NightShift/src/byui/cit260/nightShift/view/View.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public abstract class View implements ViewInterface {
     
-    private String displayMessage;
+    public String displayMessage;
     
     protected final BufferedReader keyboard = NightShift.getInFile();
     protected final PrintWriter console = NightShift.getOutFile();
@@ -27,13 +27,12 @@ public abstract class View implements ViewInterface {
     
     @Override 
     public void display() {
-
+           String value;
         boolean done = false; // set flag to not done
         do {
             // prompt for and get players name
-            String value = this.getInput();
-            if (value.toUpperCase().equals("Q")) // user wants to quit
-                return; // exit the game
+            this.console.println(this.message);
+            value = this.getInput(); // user wants to quit
             
             // do the requested action and display the next view
             done = this.doAction(value);
@@ -46,25 +45,27 @@ public abstract class View implements ViewInterface {
 
         Scanner keyboard = new Scanner(System.in); // get infile for keyboard
         boolean valid = false; // initialize to not valid
-        String value = null; // value to be returned
+        String selection = null; // value to be returned
         
         while (!valid) { // loop while valid input has not been retrieved
             
             // prompt for the player's name
             System.out.println("\n" + this.displayMessage);
             
-            value = keyboard.nextLine(); // get next line typed on keyboard
-            value = value.trim(); // trim off leading and trailing blanks
+            select = keyboard.readLine(); // get next line typed on keyboard
+            select = selection.trim(); // trim off leading and trailing blanks
             
-            if (value.length() < 1) { // value is blank
+            if (selection.length() < 1) { // value is blank
                 System.out.println("\n*** Invalid value: value cannot be blank ***");
                 continue;
             }
             
             break; // end the loop
         }
-        
-        return value; // return the value entered
+    } catch (Exception e) {
+        System.out.println("Error reading input: " + e.getMessage());
+}   
+        return selection; // return the value entered
         
             
         
