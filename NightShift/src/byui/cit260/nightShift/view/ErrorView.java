d7d3ffc9df94d312699fef9b730822844c3e130f
Silver <Silver@TRON>
2016-03-25 02:53:28 -0600

    Merge origin/master
    Conflicts:
    	NightShift/src/nightshift/NightShift.java

---
+++ NightShift/src/byui/cit260/nightShift/view/ErrorView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.nightShift.view;
+
+import java.io.PrintWriter;
+import nightshift.NightShift;
+
+/**
+ *
+ * @author Silver
+ */
+public class ErrorView {
+    
+    private static final PrintWriter errorFile = NightShift.getOutFile();
+    
+    public static void display(String className, String errorMessage){
+        
+        errorFile.println(
+                    "-------------------------------------------------"
+                  + "\n- ERROR -" + errorMessage
+                  + "\n------------------------------------------------");
+    }
+    
+    }
